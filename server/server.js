8d3f8163f19c110902a418598f0b2a576d75ca5e
phubisz <hubisz@gmail.com>
2017-07-21 22:03:24 +0200

    Add GET / todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var bodyParser = require('body-parser');
+const ObjectID = require("mongodb").ObjectID;
 
 var mongoose = require("./db/mongoose").mongoose;
 var Todo = require("./models/todo").Todo;
-var Users = require("./models/user").User;
+var User = require("./models/user").User;
 
 var app = express();
 
@@ -29,6 +30,28 @@ app.get("/todos", (req, res) => {
     res.status(400).send(e);
   })
 });
+
+
+app.get("/todos/:id", (req, res) => {
+ var id = req.params.id;
+
+if(!ObjectID.isValid(id)) {
+return res.status(404).send();
+
+}
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+    res.status(200).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+});
+
+
 
 app.listen(3000, () => {
   console.log("Started on port 3000")
